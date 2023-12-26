ffaff89cb085e72afa89fcb43c87f1a401b6c6cc
Lander Van laer <lander.vanlaer1@gmail.com>
2023-12-26 18:29:23 +0100

    Day 3 - Round 2

--- 3/Program.cs
+++ 3/Program.cs
@@ -3,28 +3,35 @@
 int sum = 0;
 
 while (file.ReadLine() is { } line)
-{
-    int middle = line.Length / 2;
-
-    string left = line[..middle];
-    string right = line[middle..];
-
-    char commonItem = FindCommonItem(left, right);
-
-    sum += commonItem - (char.ToLower(commonItem) == commonItem ? 96 : 38);
-}
+    sum += FindCommonItem(
+        line,
+        file.ReadLine() ?? throw new InvalidOperationException(),
+        file.ReadLine() ?? throw new InvalidOperationException()
+    );
 
 
 Console.WriteLine(sum);
 
 return;
 
-char FindCommonItem(string left, string right)
+int FindCommonItem(params string[] lines)
 {
-    foreach (char l in left)
-    foreach (char r in right)
-        if (l == r)
-            return l;
+    int[] chars = new int[26 * 2];
+
+    foreach (string line in lines)
+    {
+        int[] charsOfLines = new int[26 * 2];
+
+        foreach (char c in line)
+            charsOfLines[c - (char.ToLower(c) == c ? 97 : 39)] = 1;
+
+        for (int i = 0; i < chars.Length; ++i)
+            chars[i] += charsOfLines[i];
+    }
+
+    for (int i = 0; i < chars.Length; ++i)
+        if (chars[i] == lines.Length)
+            return i + 1;
 
     throw new InvalidOperationException();
 }
