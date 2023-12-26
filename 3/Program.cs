50fde516b55aac0d649b1ba40239094dae54b003
Lander Van laer <lander.vanlaer1@gmail.com>
2023-12-26 18:03:06 +0100

    Day 3 - Round 1

---
+++ 3/Program.cs
@@ -0,0 +1,30 @@
+﻿StreamReader file = new(args[0]);
+
+int sum = 0;
+
+while (file.ReadLine() is { } line)
+{
+    int middle = line.Length / 2;
+
+    string left = line[..middle];
+    string right = line[middle..];
+
+    char commonItem = FindCommonItem(left, right);
+
+    sum += commonItem - (char.ToLower(commonItem) == commonItem ? 96 : 38);
+}
+
+
+Console.WriteLine(sum);
+
+return;
+
+char FindCommonItem(string left, string right)
+{
+    foreach (char l in left)
+    foreach (char r in right)
+        if (l == r)
+            return l;
+
+    throw new InvalidOperationException();
+}
