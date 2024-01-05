a5fd061ae40257699f871b0bc2adf0066035bc09
Lander Van laer <lander.vanlaer1@gmail.com>
2024-01-05 18:18:40 +0100

    Day 6 - Round 1

---
+++ 6/Program.cs
@@ -0,0 +1,29 @@
+﻿StreamReader file = new(args[0]);
+
+char[] buffer = new char[4];
+int location;
+
+for (location = 0; location < 3; location++)
+    buffer[location] = (char)file.Read();
+
+for (char c; (c = (char)file.Read()) != '\uffff'; ++location)
+{
+    buffer[location % buffer.Length] = c;
+
+    if (!HasDuplicates(buffer)) break;
+}
+
+Console.WriteLine(string.Join("", buffer) + " after " + (location + 1) + " chars");
+
+
+return;
+
+bool HasDuplicates(IReadOnlyList<char> arr)
+{
+    for (int i = 0; i < arr.Count; i++)
+    for (int j = i + 1; j < arr.Count; j++)
+        if (arr[i] == arr[j])
+            return true;
+
+    return false;
+}
