cc97b186fb81bd755b80844567263a81f3f749d8
Lander Van laer <lander.vanlaer1@gmail.com>
2024-01-23 01:58:45 +0100

    Day 10 - Round 1

---
+++ 10/Program.cs
@@ -0,0 +1,33 @@
+﻿StreamReader file = new(args[0]);
+
+int counter = 0;
+int sum = 0;
+int signalStrength = 1;
+
+while (file.ReadLine() is { } line)
+    switch (line[0])
+    {
+        case 'n':
+            ++counter;
+            CalcSum();
+            break;
+        case 'a':
+            ++counter;
+            CalcSum();
+            ++counter;
+            CalcSum();
+
+            signalStrength += int.Parse(line[5..]);
+            break;
+        default:
+            throw new Exception();
+    }
+
+Console.WriteLine(sum);
+return;
+
+void CalcSum()
+{
+    if ((counter - 20) % 40 == 0)
+        sum += signalStrength * counter;
+}
